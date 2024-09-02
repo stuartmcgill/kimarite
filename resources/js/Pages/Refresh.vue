bc0ceb9dbedca86a74ea90cf30e4d645b4c5722d
Stuart McGill <stueymcgill@gmail.com>
2024-09-02 21:29:28 +0100

    Add Axios support

--- resources/js/Pages/Refresh.vue
+++ resources/js/Pages/Refresh.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3'
 import { useRefreshStore } from '@/stores/refresh'
+import LoadingIndicator from '@/Components/App/LoadingIndicator.vue'
 
 const store = useRefreshStore()
+
+const rebuild = () => {
+  if (window.confirm('Are you SURE you want to rebuild the Kimarite DB')) {
+    store.refresh()
+  }
+}
 </script>
 
 <template>
-  <Head title="Refresh Kimarite statistics" />
+  <Head title="Rebuild Kimarite statistics" />
   <div class="p-4 max-w-2xl container flex justify-center">
-    <div class="flex flex-col gap-2">
-      <div>Are you sure you want to refresh the Kimarite database?</div>
-      <button
-        class="p-2 flex text-white justify-center bg-red-500 rounded hover:opacity-80"
-        @click="store.refresh"
-      >
-        Refresh
-      </button>
+    <div class="flex flex-col gap-2 items-center">
+      <div>
+        This will rebuild the DB from the Sumo API site,
+        <strong>destroying any existing data</strong>.
+      </div>
+      <div class="flex gap-2">
+        <LoadingIndicator v-if="store.loading" />
+        <button
+          v-else
+          class="p-2 flex text-white justify-center bg-red-500 rounded hover:opacity-80"
+          @click="rebuild"
+        >
+          Rebuild
+        </button>
+      </div>
     </div>
   </div>
 </template>
--- resources/js/stores/refresh.ts
+++ resources/js/stores/refresh.ts
@@ -1,11 +1,20 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useRefreshStore = defineStore('refresh', {
-  state: () => ({ kimarites: [] as object[] }),
+  state: () => ({ kimarites: [] as object[], loading: false as boolean }),
   getters: {},
   actions: {
-    refresh() {
-      console.log('Hello!')
+    async refresh() {
+      this.loading = true
+      try {
+        const resp = await axios.get(
+          'https://jsonplaceholder.typicode.com/todos/1',
+        )
+        console.log(resp)
+      } finally {
+        this.loading = false
+      }
     },
   },
 })
