6673940992f1f8766a8ebc5bb7302e791248e94a
Stuart McGill <stueymcgill@gmail.com>
2025-02-01 17:32:37 +0000

    Update to Tailwind 4

--- resources/js/Components/Head2Head/SelectedWrestler.vue
+++ resources/js/Components/Head2Head/SelectedWrestler.vue
@@ -1,55 +1,55 @@
 <script setup lang="ts">
-import {computed} from "vue";
-import {WrestlerRecord} from "@/types/head2head/wrestlerRecord";
+import { computed } from 'vue'
+import { WrestlerRecord } from '@/types/head2head/wrestlerRecord'
 
 const props = defineProps<{ selected: WrestlerRecord }>()
 
 const recordLink = computed(() => {
-    if (!props.selected) {
-        return ''
-    }
+  if (!props.selected) {
+    return ''
+  }
 
-    return 'https://sumodb.sumogames.de/Rikishi.aspx?r=' + props.selected.sumoDbId
+  return 'https://sumodb.sumogames.de/Rikishi.aspx?r=' + props.selected.sumoDbId
 })
 
 const imageLink = computed(() => {
-    if (!props.selected) {
-        return ''
-    }
+  if (!props.selected) {
+    return ''
+  }
 
-    return 'https://sumodb.sumogames.de/pics/' + props.selected.sumoDbId + '.jpg'
+  return 'https://sumodb.sumogames.de/pics/' + props.selected.sumoDbId + '.jpg'
 })
 </script>
 
 <template>
-    <div class="card m-3" v-show="selected">
-        <div class="row card-body">
-            <div class="col-sm-7">
-                <h1 class="card-title">{{ selected.shikonaEn }}</h1>
-                <p class="card-text">
-                    <div class="shikona-jp">{{ selected.shikonaJp }}</div>
+  <div class="card m-3" v-show="selected">
+    <div class="row card-body">
+      <div class="col-sm-7">
+        <h1 class="card-title">{{ selected.shikonaEn }}</h1>
+        <div class="card-text">
+          <div class="shikona-jp">{{ selected.shikonaJp }}</div>
 
-                    <dl class="row">
-                        <dt class="col-sm-4">Rank</dt>
-                        <dd class="col-sm-8">{{ selected.currentRank }}</dd>
+          <dl class="row">
+            <dt class="col-sm-4">Rank</dt>
+            <dd class="col-sm-8">{{ selected.currentRank }}</dd>
 
-                        <dt class="col-sm-4">Stable</dt>
-                        <dd class="col-sm-8">{{ selected.heya }}</dd>
+            <dt class="col-sm-4">Stable</dt>
+            <dd class="col-sm-8">{{ selected.heya }}</dd>
 
-                        <dt class="col-sm-4">Height</dt>
-                        <dd class="col-sm-8">{{ selected.height }}cm</dd>
+            <dt class="col-sm-4">Height</dt>
+            <dd class="col-sm-8">{{ selected.height }}cm</dd>
 
-                        <dt class="col-sm-4">Weight</dt>
-                        <dd class="col-sm-8">{{ selected.weight }}kg</dd>
-                    </dl>
-                </p>
-                <a :href="recordLink" class="btn btn-primary">Full record</a>
-            </div>
-            <img
-                :src="imageLink"
-                class="selected-wrestler-image col-sm-5 img-responsive object-contain"
-                :alt="'Photograph of ' + selected.shikonaEn"
-            >
+            <dt class="col-sm-4">Weight</dt>
+            <dd class="col-sm-8">{{ selected.weight }}kg</dd>
+          </dl>
         </div>
+        <a :href="recordLink" class="btn btn-primary">Full record</a>
+      </div>
+      <img
+        :src="imageLink"
+        class="selected-wrestler-image col-sm-5 img-responsive object-contain"
+        :alt="'Photograph of ' + selected.shikonaEn"
+      />
     </div>
+  </div>
 </template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -43,7 +43,7 @@ const showingNavigationDropdown = ref(false);
                                         <span class="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-hidden transition ease-in-out duration-150"
                                             >
                                                 {{ $page.props.auth.user.name }}
 
@@ -77,7 +77,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="-me-2 flex items-center sm:hidden">
                             <button
                                 @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
                                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path
@@ -137,7 +137,7 @@ const showingNavigationDropdown = ref(false);
             </nav>
 
             <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
+            <header class="bg-white shadow-sm" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
